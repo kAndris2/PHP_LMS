87ae9f2ff981009ba849ce56f991fde300248629
root <root@lrvalte.mradmin.hu>
2020-08-18 11:50:40 +0000

    fix progressbar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -139,7 +139,7 @@
     <div class="content">
       <div class="container-fluid">
         <router-view></router-view>
-        <vue-progress-bar></vue-progress-bar>
+
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
