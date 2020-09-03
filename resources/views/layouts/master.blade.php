34311d937516a045629a40ec3f605c7509eed252
andri <andri@DESKTOP-FIORF47>
2020-09-03 12:34:25 +0200

    dd

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -77,7 +77,7 @@
           </li>
             <!-- Profile -->
           <li class="nav-item">
-            <router-link to='{{ "profile/".Auth::user()->id }}' class="nav-link">
+            <router-link to='{{ "/profile/".Auth::user()->id }}' class="nav-link">
               <i class="nav-icon fas fa-user"></i>
               <p>
                 Profile
